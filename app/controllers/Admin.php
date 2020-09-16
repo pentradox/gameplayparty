e6524bbc0f4ccf3cd5cfbc35a7120f8c93f6535e
AnonymousSmurf <45464235+AnonymousSmurf@users.noreply.github.com>
2020-09-16 13:47:22 +0200

    Login redirect
    When loged in you will always be redirected to the dashboard if you want to go back to login

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -10,27 +10,27 @@ class Admin extends controller {
     $password = trim($_POST['password']);
     $login = $this->loginModel->LoginCheck($username, $password);
     if($login == true) {
-      $this->redirect("home");
+      $this->redirect("Dashboard");
     } else {
-      $this->redirect("admin");
+      $this->redirect("Admin");
     }
   }
 
   public function index() {
-    $this->view("pages/login");
+    if (isset($_SESSION["userid"])) {
+      if ($_SESSION["userid"] != null) {
+        $this->redirect("Dashboard");
+      } else {
+        $this->view("pages/login");
+      }
+    } else {
+      $this->view("pages/login");
+    } 
   }
 
   public function logout() {
     session_unset();
     $this->redirect("home");
-  }
-
-  public function dashboard() {
-    if ($_SESSION["userid"] != null) {
-      $this->view("pages/dashboard");
-    } else {
-      $this->redirect("admin");
-    }
   }
 
 }
