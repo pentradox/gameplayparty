1a986b3d197a48cd6b844834e313693d66d54165
AnonymousSmurf <45464235+AnonymousSmurf@users.noreply.github.com>
2020-09-16 18:58:16 +0200

    Added begining of dashboard

--- app/views/pages/dashboard.php
+++ app/views/pages/dashboard.php
@@ -1,5 +1,12 @@
-<?php include APPROOT."/views/fragments/navbar.php"; ?>
-
-works
+<div class="container-dashboard">
+    <?php include APPROOT."/views/fragments/dashboardNav.php"; ?>
+        <main>
+            <div class="intro">
+                <div class="text-container">
+                    <h1>Welcome <?php echo $_SESSION["userid"] ?></h1>
+                </div>
+            </div>
+        </main>
+     </div>
 
 <?php include APPROOT."/views/fragments/footer.php"; ?>
