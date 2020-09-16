18d876edf391f71e29bcdd93134b05dd8145e001
AnonymousSmurf <45464235+AnonymousSmurf@users.noreply.github.com>
2020-09-16 19:55:15 +0200

    Updated error handling for login
    - added messages

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -1,31 +1,38 @@
 <?php
 class Admin extends controller {
-
+  
   public function __construct() {
     $this->loginModel = $this->model("Login");
   }
 
-  public function loginCheck() {
+  public function loginCheck() {  
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
     $login = $this->loginModel->LoginCheck($username, $password);
-    if($login == true) {
+    if($login == null) {
       $this->redirect("Dashboard");
     } else {
-      $this->redirect("Admin");
+      $data = [
+        "error" => $login
+      ];
+      $this->view("pages/login", $data);
     }
   }
 
   public function index() {
-    if (isset($_SESSION["userid"])) {
-      if ($_SESSION["userid"] != null) {
-        $this->redirect("Dashboard");
+    if ((isset($_POST["username"])) && (isset($_POST["password"]))) {
+      $this->loginCheck();
+    } else {
+      if (isset($_SESSION["userid"])) {
+        if ($_SESSION["userid"] != null) {
+          $this->redirect("Dashboard");
+        } else {
+          $this->view("pages/login");
+        }
       } else {
         $this->view("pages/login");
-      }
-    } else {
-      $this->view("pages/login");
-    } 
+      } 
+    }
   }
 
   public function logout() {
