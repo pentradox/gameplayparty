20cd6992da8dc289e0bd00f3d1d57a4cf3046139
Dymaxx <remcosmurf1@gmail.com>
2020-09-25 13:48:05 +0200

    cookie stuf

---
+++ app/views/pages/cookie.php
@@ -0,0 +1,18 @@
+<?php include APPROOT."/views/fragments/navbar.php"; ?>
+
+<div class="jumbotron h-100">
+  <div id="hero-text">
+    <h4 class="text-lg text-center text-uppercase font-weight-bold">
+      Game Play Party
+    </h1>
+    <p class="lead text-center">Power to the players</p>
+  </div>
+</div>
+
+
+<main class="container mt-5">
+<?php include APPROOT."/views/fragments/termsfeed-privacy-policy-html-dutch.html"; ?>
+  
+</main>
+
+<?php include APPROOT."/views/fragments/footer.php"; ?>
