f8a1cf661301bb00ab9e78259b76ca930c68c3ff
AnonymousSmurf <45464235+AnonymousSmurf@users.noreply.github.com>
2020-09-25 22:32:43 +0200

    cleaned login

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -7,91 +7,77 @@ class Dashboard extends controller {
   }
 
   public function index() {
-    if ($_SESSION["userid"] != null) {
-      $this->view("pages/dashboard");
-    } else {
-      $this->redirect("Userlogin");
-    }
+    $this->sessionCheck();
+    $this->view("pages/dashboard");
   }
 
   public function halls($data = null) {
-    if ($_SESSION["userid"] != null) {
-      $data["halls"] = $this->hallsModel->getAllHalls();
-      $this->view("Pages/halls", $data);
-    }
+    $this->sessionCheck();
+    $data["halls"] = $this->hallsModel->getAllHalls();
+    $this->view("Pages/halls", $data);
   }
 
   public function acounts() {
+    $this->sessionCheck();
     $this->view("Pages/acounts");
   }
 
   public function updatehall($id = null) {
-    if ($_SESSION["userid"] != null) {
-      if (isset($id)) {
-        $data = $this->hallsModel->getHall($id);
-        if ($data["hall_error"] != null) {
-          $data["halls"] = $this->hallsModel->getAllHalls();
-          $this->view("pages/halls",$data);
-        } else {
-          $this->view("pages/updateHall",$data);
-        }
+    $this->sessionCheck();
+    if (isset($id)) {
+      $data = $this->hallsModel->getHall($id);
+      if ($data["hall_error"] != null) {
+        $data["halls"] = $this->hallsModel->getAllHalls();
+        $this->view("pages/halls",$data);
       } else {
-        $data = $this->hallsModel->sendUpdateHall();
-        $data2 = $this->hallsModel->getHall($data["id"]);
-        $data["hall"] = $data2["hall"];
-        if ($data["success_message"] != null) {
-          $this->view("pages/updateHall",$data);
-        } else {
-          $this->view("pages/updateHall",$data);
-        }
+        $this->view("pages/updateHall",$data);
       }
     } else {
-      $this->redirect("Userlogin");
+      $data = $this->hallsModel->sendUpdateHall();
+      $data2 = $this->hallsModel->getHall($data["id"]);
+      $data["hall"] = $data2["hall"];
+      if ($data["success_message"] != null) {
+        $this->view("pages/updateHall",$data);
+      } else {
+        $this->view("pages/updateHall",$data);
+      }
     }
   }
 
   public function deletehall($id = null) {
-    if ($_SESSION["userid"] != null) {
-      if ($id != null) {
-        $delete = $this->hallsModel->deletehall($id);
-        $data["halls"] = $this->hallsModel->getAllHalls();
-        if ($delete) {
-          $data["hall_error"] = "Zaal succesvol verwijderd";
-          $this->view("pages/halls",$data);
-        } else {
-          $data["hall_error"] = "Error zaal bestaat niet!";
-          $this->updatehall("pages/halls",$data);
-        }
-      } else {
-        $this->redirect("Dashboard/halls");
-      }
+    $this->sessionCheck();
+    $data = [
+      "hall_error" => null
+    ];
+    $data["hall_error"] = $this->hallsModel->deletehall($id,$data);
+    if ($data) {
+      $data["halls"] = $this->hallsModel->getAllHalls();  
+      $this->updatehall("pages/halls",$data);
     } else {
-      $this->redirect("Userlogin");
+      $this->redirect("Dashboard/halls");
     }
   }
 
   public function createhall() {
-    if ($_SESSION["userid"] != null) {
-      $data = [
-        "hall_number_error" => null,
-        "hall_seats_error" => null,
-        "hall_sound_error" => null,
-        "error" => null,
-        "success_message" => null
-      ];
-      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $data = $this->hallsModel->createHall($data);
-        $this->view("pages/createHall",$data);
-      } else {
-        $this->view("pages/createHall",$data);
-      }
+    $this->sessionCheck();
+    $data = [
+      "hall_number_error" => null,
+      "hall_seats_error" => null,
+      "hall_sound_error" => null,
+      "error" => null,
+      "success_message" => null
+    ];
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $data = $this->hallsModel->createHall($data);
+      $this->view("pages/createHall",$data);
     } else {
-      $this->redirect("Userlogin");
+      $this->view("pages/createHall",$data);
     }
   }
 
 
   public function createPacket() {
+    $this->sessionCheck();
     $this->view("Pages/createPacket");
   }
 
