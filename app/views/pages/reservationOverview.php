34760658f730e37736b9f7758ac23f66238c78df
S3ntrail <46055932+S3ntrail@users.noreply.github.com>
2020-11-02 18:38:41 +0100

    Fixing small mistakes

--- app/views/pages/reservationOverview.php
+++ app/views/pages/reservationOverview.php
@@ -56,54 +56,10 @@
 </div>
 
 <?php
-if (isset($data["users"])) {
-  foreach ($data["users"] as $user) {
+if (isset($data["reservation"])) {
+  foreach ($data["reservation"] as $reservation) {
 
-    if ($user->active == 1) {
-
-      echo '<div class="modal fade" id="de-active' . $user->id . '" tabindex="-1" role="dialog" aria-labelledby="de-active' . $user->id . '" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="de-active">Weet U het zeker?</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              Als u deze bioscoop deactiveert dan kan de gebruiker geen zalen en producten op de website aanmaken!
-              Dus weet het zeker dat dit het juiste account is? 
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Nee</button>
-              <a href="' . URLROOT . "/Dashboard/activate/" . $user->id . '"><button type="button" class="btn btn-danger">Deactiveren</button></a>
-            </div>
-          </div>
-        </div>
-      </div>';
-    } else {
-      echo '<div class="modal fade" id="active" tabindex="-1" role="dialog" aria-labelledby="active" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="active">Weet U het zeker?</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              Als u deze bioscoop activeert dan kan de gebruiker zaalen en producten op de website aanmaken!
-              Dus weet het zeker dat dit het juiste account is? 
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Nee</button>
-              <a href="' . URLROOT . "/Dashboard/activate/" . $user->id . '"><button type="button" class="btn btn-success">Activeren</button></a>
-            </div>
-          </div>
-        </div>
-      </div>';
-    }
-    echo '<div class="modal fade" id="delete' . $user->id . '" tabindex="-1" role="dialog" aria-labelledby="delete' . $user->id . '" aria-hidden="true">
+    echo '<div class="modal fade" id="delete' . $reservation->id . '" tabindex="-1" role="dialog" aria-labelledby="delete' . $reservation->id . '" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -113,12 +69,11 @@ if (isset($data["users"])) {
             </button>
           </div>
           <div class="modal-body">
-            Als u deze bioscoop verwijderd kunt u deze niet meer terug krijgen.
-            Weet u ook zeker dat dit niet de verkeerde biscoop is en dat de klant op te hoogte?
+            Weet u het dat u de reservering wilt verwijderen?
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Nee</button>
-            <a href="' . URLROOT . "/Dashboard/deleteUser/" . $user->id . '"><button type="button" class="btn btn-danger">Verwijder</button></a>
+            <a href="' . URLROOT . "/Dashboard/deleteReservation/" . $reservation->id . '"><button type="button" class="btn btn-danger">Verwijder</button></a>
           </div>
         </div>
       </div>
